63eaac460f7146ebea507a3bf48db31d55b26f20
leminhtam221 <le.minhtam221@gmail.com>
2020-08-17 18:24:30 +0700

    update server

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ mongoose
   .catch(console.log);
 
 const app = express();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, token"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
+  next();
+});
+
 app.use(express.json());
 
 app.use("/images", express.static("./uploads"));
